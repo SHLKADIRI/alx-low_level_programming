b7e605e0391690d44863a7a9c56369396ff0be5a
Souhail KADIRI <souhailkadiri95@gmail.com>
2023-03-16 09:42:44 -0700

    8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void) {
+  const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+  const int message_length = 50;
+  const int stderr_fd = 2;
+
+  write(stderr_fd, message, message_length);
+
+  return 1;
+}
