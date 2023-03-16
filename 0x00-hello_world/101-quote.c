0cfd61a2ee88d0c4cf2a7490dc83c74503ad3543
Souhail KADIRI <souhailkadiri95@gmail.com>
2023-03-16 09:58:26 -0700

    8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,9 @@
 #include <unistd.h>
 
 /**
- * main - Entry point
+ * main - A c program that prints a line to the standard error
  *
- * Return: Always 1 (Success)
+ * Return 1 (Success)
  */
 
 int main(void)
