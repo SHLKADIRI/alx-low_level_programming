ec08afdbdec7592e7e40ccb86d015e96ae37152c
Souhail KADIRI <souhailkadiri95@gmail.com>
2023-03-17 15:53:03 -0700

    101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,11 +14,11 @@ int main(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = i; j <= 9; j++)
+		for (j = 1; j <= 9; j++)
 		{
-			for (k = j; k <= 9; k++)
+			for (k = 2; k <= 9; k++)
 			{
-				if (i < j && j < k && i != j && j != k)
+				if (i < j && j < k)
 				{
 					putchar(i + '0');
 					putchar(j + '0');
