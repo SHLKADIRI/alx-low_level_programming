06b635e02b575ff2289bfac5ad3bac0be5180baa
Souhail KADIRI <souhailkadiri95@gmail.com>
2023-03-17 15:29:54 -0700

    100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,9 +15,16 @@ int main(void)
 	{
 		for (j = i; j <=9; j++)
 		{
-			putchar(i + '0');
-			putchar(j + '0');
-			putchar(', ');
+			if (i < j && i != j)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				if (i + j != 17)
+				{
+					putchar(', ');
+					putchar(' ');
+				}
+			}
 		}
 	}
 	putchar('\n');
