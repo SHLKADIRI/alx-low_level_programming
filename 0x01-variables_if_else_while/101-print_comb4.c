a5a6f8c2b31eda38702be32eaad1ef9cc6466df7
Souhail KADIRI <souhailkadiri95@gmail.com>
2023-03-17 15:43:30 -0700

    1011

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = i; j <= 9; j++)
+		{
+			for (k = j; k <= 9; k++)
+			{
+				if (i < j && j < k && i != j && j != k)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(k + 'O');
+					if (i + j + k != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
