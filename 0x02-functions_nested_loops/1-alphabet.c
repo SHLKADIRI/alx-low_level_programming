2ff875bb3b06d99348d11907b9bc96c7d8fde526
Souhail KADIRI <souhailkadiri95@gmail.com>
2023-03-21 04:28:14 -0700

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * print_alphabet - function that prints alphabet from a to z
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
 	char i;
+
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
