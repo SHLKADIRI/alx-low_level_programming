76cc659b257654b5eea7eef13c3100beb4a22dae
Souhail KADIRI <souhailkadiri95@gmail.com>
2023-03-21 09:30:57 -0700

    7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,13 +2,14 @@
 
 
 /**
- * _abs - computes the absolute value of an integer
+ * print_last_digit - computes the absolute value of an integer
  * @i: the int to check
  * Return: the absolute value of int
  */
 int print_last_digit(int i)
 {
 	int j;
+
 	if (i < 0)
 	{
 		i = -(i);
