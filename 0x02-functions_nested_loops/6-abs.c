27243a689bd487b847c8e478c7c719b33a465879
Souhail KADIRI <souhailkadiri95@gmail.com>
2023-03-21 09:02:16 -0700

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - function that checks if a charcater 
+ * Return: 1 is n is positive
+ * and -1 if it's negative
+ */
+int _abs(int)
+{
+	int n;
+
+	if (n > 0)
+	{
+		return (a);
+	}
+	else if (n < 0)
+	{
+		return (-a);
+	}
+}
