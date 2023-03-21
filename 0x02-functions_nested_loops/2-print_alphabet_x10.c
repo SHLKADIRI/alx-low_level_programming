b470d1f8089362119e52cc9d01b85549cb5be913
Souhail KADIRI <souhailkadiri95@gmail.com>
2023-03-21 04:38:24 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function that prints alphabet from a to z
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 1; n <= 10; n++)
+	{
+		void print_alphabet(void);
+	}
+	_putchar('\n');
+}
