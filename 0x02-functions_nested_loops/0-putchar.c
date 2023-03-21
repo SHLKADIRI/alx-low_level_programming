1a37c18319157d2ee4eb238f5a4029be9b679e10
Souhail KADIRI <souhailkadiri95@gmail.com>
2023-03-21 04:00:59 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,10 +2,9 @@
 
 /**
  * main - Entry point
- * Return: putchar
+ * 
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	_putchar('_');
@@ -17,6 +16,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return(0);
+	return (0);
 	
 }
