e5e00013e21af3c9cbc540115d9b3a0fab8807a3
Souhail KADIRI <souhailkadiri95@gmail.com>
2023-03-21 06:58:45 -0700

    2

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function that prints alphabet from a to z
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+	if (i >= 'a' && i <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
