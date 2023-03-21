46ff6965cc9dce3e58fa672c0db336cd29f456ad
Souhail KADIRI <souhailkadiri95@gmail.com>
2023-03-21 08:01:44 -0700

    5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,19 +1,36 @@
 #include "main.h"
 
 /**
- * _islower - function that checks if a charcater is a lowercase or not
- * @c: parameter
- * Return: 1 is c is lowercase
- * and 0 if it's not
+ * _print_sign - function that checks if a charcater is positive, zero or negative
+ * @n: parameter
+ * Return: 1 is n is positive
+ * and 0 if it(s equal to zero
+ * and -1 if it's negative
  */
-int _islower(int c)
+int _print_sign(int n)
 {
-	if (c >= 'a' && c <= 'z')
+	if (n > 0)
 	{
 		return (1);
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+		_putchar('\n');
 	}
-	else
+	else if (n == 0)
 	{
 		return (0);
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		_putchar('\n');
+        }
+	else
+	{
+		return (-1);
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
+		_putchar('\n');
 	}
 }
