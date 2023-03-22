2986a7bc664b24cd14bd8be149022c6a1f815064
SHLKADIRI <souhailkadiri95@gmail.com>
2023-03-22 15:54:55 +0000

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,7 +2,6 @@
 
 /**
 * main - tests function that prints if integer is positive or negative
-* @i: parameter to be checked
 * Return: 0
 */
 int main(void)
@@ -10,7 +9,6 @@ int main(void)
 	int i;
 
 	i = 0;
-
 	positive_or_negative(i);
 	return (0);
 }
