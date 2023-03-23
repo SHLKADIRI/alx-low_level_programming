28639aa3e25387e10221692204267fd56fad5c92
SHLKADIRI <souhailkadiri95@gmail.com>
2023-03-23 13:21:20 +0000

    Task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - function that checks if a charcater is an alphabet or not
+ * @c: parameter
+ * Return: 1 is c is an alphabet
+ * and 0 if it's not
+ */
+int _isdigit(int c)
+{
+	if (c >= 80 && c <= 89)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
