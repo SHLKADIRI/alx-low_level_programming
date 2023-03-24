f1a71d611a7a39c0abf18aaf08945e30e0b1019b
SHLKADIRI <souhailkadiri95@gmail.com>
2023-03-24 01:24:56 +0000

    Task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,17 +2,21 @@
 
 /**
  * print_line - function that draws a straight line
+ * @n: parameter of how many time to print _
  * Return: void
  */
 void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i <= n; i++)
-	{
-		if (n > 0)
-			_putchar('_');
+	if (n < 0)
 		_putchar('\n');
-
+	else
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
 	}
 }
