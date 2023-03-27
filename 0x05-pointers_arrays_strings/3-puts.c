144c54477ee0ddabdd21df2e46eb6c0b4cb34d8a
SHLKADIRI <souhailkadiri95@gmail.com>
2023-03-27 14:07:56 +0000

    Task3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - function that pointer to an int
+ * @str: string to print
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
+
