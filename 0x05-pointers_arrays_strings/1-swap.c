b58f3f380c75c5525024d0404c5ef074b98c5ee9
SHLKADIRI <souhailkadiri95@gmail.com>
2023-03-27 13:29:02 +0000

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+
+#include "main.h"
+
+/**
+ * reset_to_98 - function that pointer to an int
+ * @a: first input
+ * @b: second input
+ * Return: a and b
+ */
+void swap_int(int *a, int *b)
+{
+	*a = b;
+	*b = a;
+}
+
