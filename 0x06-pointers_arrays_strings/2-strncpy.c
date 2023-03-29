791038432a4a67323bd0d84b2fde0048f32fa048
SHLKADIRI <souhailkadiri95@gmail.com>
2023-03-29 16:01:25 +0000

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,14 +5,12 @@
  * @dest: first string
  * @src: second string
  * @n: charcter
- * Return: void
+ * Return: dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-
-	while (dest[i] != '\0')
-		i++;
+	
 	i = 0;
 	while (i < n && src[i] != '\0')
 	{
