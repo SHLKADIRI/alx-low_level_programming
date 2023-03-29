eaf5fd693186d5a3158e7d138d332519ef22bfd5
SHLKADIRI <souhailkadiri95@gmail.com>
2023-03-29 20:15:38 +0000

    Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcmp - function that compare two string
+ * @s1: first string
+ * @s2: second string
+ * Return: void
+ */
+char *_strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] == s2[i])
+	{
+		if (s1[i] == '\0' || s2[i] == '\0')
+			break;
+		i++;
+	}
+	if (s1[i] == '\0' && s2[i] == '\0')
+		return (1);
+	else
+		return (0);
+}
