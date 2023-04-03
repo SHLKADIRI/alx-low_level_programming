19671b42bb90f5313f912d73816c3f6364bd39fc
SHLKADIRI <souhailkadiri95@gmail.com>
2023-04-03 14:14:30 +0000

    Advancedntasks

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: pointer to pointer
+ * @to: char pointer
+ */
+void set_string(char **s, chat *to)
+{
+	*s = to;
+}
