a38dbdd59fc50e8187311afc850709a5ff0cef95
SHLKADIRI <souhailkadiri95@gmail.com>
2023-04-03 12:30:05 +0000

    More pointers

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - a function to fill a block of memory with a spc value
+ * @s: starting adress
+ * @b: value to be assigned
+ * @n: number of bytes
+ *
+ * Return: changed array with the new value
+ */
+char *_memset(char *s, char b, unsignedd int n)
+{
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
