6b87a4ae0f7882d533bc31ccdc229ec0f930e1cf
SHLKADIRI <souhailkadiri95@gmail.com>
2023-04-05 12:28:45 +0000

    Third recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - a fucntion to print the lenght of a string
+ * @s: input
+ * Return : the lenght
+ */
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s)
+	{
+		len++;
+		len = len + _strlen_recursion(s + 1);
+	}
+	return (len);
+}
