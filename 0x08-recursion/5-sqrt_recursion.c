42013cef59473972a59cc5c42f4f2baacf2e8f63
SHLKADIRI <souhailkadiri95@gmail.com>
2023-04-05 13:20:28 +0000

    7th recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - fucntion to calculate the root square of a number
+ * @n: input
+ * Return: the root squre
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (a_s_r(n, 0));
+}
+/**
+ * a_s_r - recurses to find the natural s n
+ * @n: the number we want his square root
+ * @i: iterator
+ * Return: the result
+ */
+int a_s_r(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (a_s_r(n, i + 1));
+}
