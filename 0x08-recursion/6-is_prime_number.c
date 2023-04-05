32160aa6703ee59bc04c0504e3fa3633dc68c3c9
SHLKADIRI <souhailkadiri95@gmail.com>
2023-04-05 13:49:07 +0000

    8th recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+int a_p(int n, int i);
+
+/**
+ * is_prime_number - function to check if a integer is a pm
+ * @n: number to check
+ * Return: 1 if n is a pm, 0 if not
+ */
+int is_prime_number(int n)
+{
+	if (n <= 0)
+		return (0);
+	return (a_p(n, n - 1));
+}
+/**
+ * a_p - calculate if a number is prime or not
+ * @n: number to check
+ * @i: iterrator
+ * Return: 1 if n is prime, 0 if not
+ */
+int a_p(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (O);
+	return (a_p(n, i - 1));
+}
