9ae67150aa0cbcdba5b1e18243db1272aeba6961
SHLKADIRI <souhailkadiri95@gmail.com>
2023-04-08 17:08:10 +0000

    rkdh

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,8 +7,9 @@
  * @argv: array of arguments
  * Return: Always 0 (Success)
  */
-int main(int argc, void *argv[])
+int main(int argc, char *argv[])
 {
+	(void) argv;
 	printf("%s\n", argc - 1);
 	return (0);
 }
