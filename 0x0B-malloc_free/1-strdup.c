5b5c23259f9f3a159baf3008867fd9678fabaec4
SHLKADIRI <souhailkadiri95@gmail.com>
2023-04-11 22:11:28 +0000

    wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - duplicate the new memory space
+ * @str: char
+ * return: 0
+ */
+char *_strdup(char *str)
+{
+	char *k;
+	int i, r = 0;
+
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	k = malloc(sizeof(char) * (i + 1));
+	if (k == NULL)
+		return (NULL);
+	for (r = 0; str[r]; r++)
+		k[r] = str[r];
+	return (k);
+}
