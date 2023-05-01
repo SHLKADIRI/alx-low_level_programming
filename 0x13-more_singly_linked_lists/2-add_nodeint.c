3f2235efa5ef1610026f431de61e7bcdaf076d0a
SHLKADIRI <souhailkadiri95@gmail.com>
2023-05-02 00:25:58 +0100

    jgd

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * add_nodeint – ajot chi haja f chi haja
+ * @head: pointer l awl node
+ * @n: data li tzid f nex node
+ * Return: pointer to the new node, or NULL if it fails
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
